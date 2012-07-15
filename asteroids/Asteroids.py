cd3ff662091e6cd5bd2e5ac40e1512fa853f83de
Dan Shea <daniel.john.shea@gmail.com>
2012-07-15 17:22:42 +0900

    2012-07-15: added collision detection to asteroids, game needs some fine tuning and the addition of scoring/level mechanism, plus player respawn needs to be fixed to not respawn and die immediately due to asteroid being at the respawn point.

--- asteroids/Asteroids.py
+++ asteroids/Asteroids.py
@@ -15,14 +15,24 @@ class BoardObject(object):
         self.heading = Vector.Vector(speed, bearing)
         self.orientation = 0.0
         self.size = 10.0
+        # 4 points of the hitbox for collision detection
+        # top left, bottom left, top right, bottom right
+        self.hitbox = (((self.xpos-self.size)%self.xmax, (self.ypos-self.size)%self.ymax),
+                       ((self.xpos-self.size)%self.xmax, (self.ypos+self.size)%self.ymax),
+                       ((self.xpos+self.size)%self.xmax, (self.ypos-self.size)%self.ymax),
+                       ((self.xpos+self.size)%self.xmax, (self.ypos+self.size)%self.ymax),)
 
     def update(self):
         '''
         Given the current (x,y) position and the heading, calculate the next
-        (x,y) position.
+        (x,y) position.  Update the hitbox coordinates.
         '''
         self.xpos = (self.xpos + self.heading.x) % self.xmax
         self.ypos = (self.ypos + self.heading.y) % self.ymax
+        self.hitbox = (((self.xpos-self.size)%self.xmax, (self.ypos-self.size)%self.ymax),
+                       ((self.xpos-self.size)%self.xmax, (self.ypos+self.size)%self.ymax),
+                       ((self.xpos+self.size)%self.xmax, (self.ypos-self.size)%self.ymax),
+                       ((self.xpos+self.size)%self.xmax, (self.ypos+self.size)%self.ymax),)
 
     def draw(self):
         return pygame.draw.polygon
@@ -34,6 +44,7 @@ class Bullet(BoardObject):
         BoardObject.__init__(self, xmax, ymax, xpos, ypos, speed+Bullet.SPEED, bearing)
         self.ttl = Bullet.TTL
         self.orientation = bearing
+        self.size = 2
 
     def update(self):
         BoardObject.update(self)
@@ -50,11 +61,12 @@ class Bullet(BoardObject):
 class Asteroid(BoardObject):
     SPEED = 1
     SPAWN_SIZE = 40
+    SCORE = 100
     def __init__(self, xmax, ymax, xpos, ypos, speed, bearing):
         BoardObject.__init__(self, xmax, ymax, xpos, ypos, speed, bearing)
         self.size = Asteroid.SPAWN_SIZE
-        self.heading = Vector.Vector(random.randint(1,5)/2.0, random.randint(0,359))
         self.orientation = random.randint(0,359)
+        self.score = Asteroid.SCORE
 
     def points(self):
         p1    = Vector.Vector(self.size, self.orientation)
@@ -70,10 +82,11 @@ class Asteroid(BoardObject):
 
 class Ship(BoardObject):
     THRUST = 0.5
-    ROTATE = 10
+    ROTATE = 5
     def __init__(self, xmax, ymax, xpos, ypos, speed, bearing):
         BoardObject.__init__(self, xmax, ymax, xpos, ypos, speed, bearing)
         self.weapon = Bullet
+        self.lives = 2
 
     def thrust(self):
         self.heading = self.heading + Vector.Vector(Ship.THRUST, self.orientation)
@@ -102,40 +115,133 @@ class Ship(BoardObject):
         speed = Vector.Vector(0, self.orientation) + self.heading
         return self.weapon(self.xmax, self.ymax, self.xpos, self.ypos, speed.r, self.orientation)
 
+    def respawn(self):
+        self.xpos = self.xmax / 2
+        self.ypos = self.ymax / 2
+        self.orientation = 0.0
+        self.heading = Vector.Vector(0, 0)
+
+class Board(object):
+
+    def __init__(self):
+        self.board = list()
+        self.level = 1
+        self.score = 0
+
+    def game_over(self):
+        '''game is over'''
+        pass
+
+    def collision_check(self, A, B):
+        '''collision_check - if the hitboxes of A and B overlap, return True as a collision has occurred.'''
+        for point in A.hitbox:
+            if B.hitbox[0][0] <= point[0] <= B.hitbox[3][0]:
+                if B.hitbox[0][1] <= point[1] <= B.hitbox[3][1]:
+                    return True
+        return False
+
+    def detect_collisions(self):
+        to_remove = set()
+        to_add = list()
+        for i in self.board:
+            for j in self.board:
+                if self.collision_check(i, j) == True:
+                    if (type(i) is Ship and type(j) is Asteroid) or (type(i) is Asteroid and type(j) is Ship):
+                        if type(i) is Ship:
+                            i.lives -= 1
+                            if i.lives < 0:
+                                self.game_over()
+                            else:
+                                i.respawn()
+                        else:
+                            j.lives -= 1
+                            if j.lives < 0:
+                                self.game_over()
+                            else:
+                                j.respawn()
+                    elif (type(i) is Bullet and type(j) is Asteroid) or (type(i) is Bullet and type(j) is Asteroid):
+                        to_remove.add(i)
+                        to_remove.add(j)
+                        new_heading = i.heading + j.heading
+                        if type(i) is Asteroid:
+                            self.score += i.score
+                            new_heading.r = i.heading.r
+                            if i.size / 2 > 2:
+                                lchild = Asteroid(i.xmax, i.ymax, i.xpos, i.ypos, new_heading.r, new_heading.theta)
+                                rchild = Asteroid(i.xmax, i.ymax, i.xpos, i.ypos, new_heading.r, -new_heading.theta)
+                                lchild.score = i.score * 2
+                                rchild.score = i.score * 2
+                                lchild.size = i.size / 2
+                                rchild.size = i.size / 2
+                                speed_split = (random.randint(5,95) / 100.0)
+                                lchild.heading.r = i.heading.r * speed_split
+                                rchild.heading.r = i.heading.r * (1 - speed_split)
+                                to_add.append(lchild)
+                                to_add.append(rchild)
+                        elif j.size/2 > 2:
+                            self.score += j.score
+                            lchild = Asteroid(j.xmax, j.ymax, j.xpos, j.ypos, new_heading.r, new_heading.theta)
+                            rchild = Asteroid(j.xmax, j.ymax, j.xpos, j.ypos, new_heading.r, -new_heading.theta)
+                            lchild.score = j.score * 2
+                            rchild.score = j.score * 2
+                            lchild.size = j.size / 2
+                            rchild.size = j.size / 2
+                            speed_split = (random.randint(5,95) / 100.0)
+                            lchild.heading.r = j.heading.r * speed_split
+                            rchild.heading.r = j.heading.r * (1 - speed_split)
+                            to_add.append(lchild)
+                            to_add.append(rchild)
+        for i in to_remove:
+            self.board.remove(i)
+        for i in to_add:
+            self.board.append(i)
+
 if __name__ == '__main__':
     pygame.init()
     fpsClock = pygame.time.Clock()
     pygame.display.set_caption('Asteroids')
+    pygame.key.set_repeat(150,50)
     BLACK = pygame.Color(0,0,0)
     WHITE = pygame.Color(255,255,255)
     RED = pygame.Color(255,0,0)
-    xmax = 640
-    ymax = 480
+    xmax = 800
+    ymax = 600
     screen = pygame.display.set_mode((xmax,ymax))
     xcenter = xmax/2
     ycenter = ymax/2
 
     myShip = Ship(xmax, ymax, xcenter, ycenter, 0.0, 0.0)
-    Board = list()
-    Board.append(myShip)
-    for i in xrange(5):
-        Board.append(Asteroid(xmax, ymax, random.randint(0,xmax), random.randint(0, ymax), 0.0, 0.0))
+    board = Board()
+    board.board.append(myShip)
+    for i in xrange(board.level):
+        board.board.append(Asteroid(xmax, ymax, random.randint(0, xmax/2 - xmax/4), random.randint(0, ymax/2 - ymax/4), random.randint(1,5)/2.0, random.randint(0,359)))
+        board.board.append(Asteroid(xmax, ymax, random.randint(0, xmax/2 - xmax/4), random.randint(ymax/2 + ymax/4, ymax), random.randint(1,5)/2.0, random.randint(0,359)))
+        board.board.append(Asteroid(xmax, ymax, random.randint(xmax/2 + xmax/4, xmax), random.randint(0, ymax/2 - ymax/4), random.randint(1,5)/2.0, random.randint(0,359)))
+        board.board.append(Asteroid(xmax, ymax, random.randint(xmax/2 + xmax/4, xmax), random.randint(ymax/2 + ymax/4, ymax), random.randint(1,5)/2.0, random.randint(0,359)))
 
     while True:
         screen.fill(BLACK)
 
-        index = 0
-        for i in Board:
+        for i in board.board:
+            # update positions on the board
             i.update()
+
+        # perform collision detection
+        board.detect_collisions()
+
+        # redraw the board
+        index = 0
+        for i in board.board:
             if type(i) is Bullet:
                 if i.ttl > 0:
                     i.draw()(screen, RED, i.points(), 1)
                 else:
-                    Board.pop(index)
+                    board.board.pop(index)
             else:
                 i.draw()(screen, WHITE, i.points(), 1)
             index += 1
 
+        # capture keyboard events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -150,6 +256,6 @@ if __name__ == '__main__':
                 elif event.key == pygame.K_DOWN:
                     myShip.reversethrust()
                 elif event.key == pygame.K_SPACE:
-                    Board.append(myShip.shoot())
+                    board.board.append(myShip.shoot())
         pygame.display.update()
         fpsClock.tick(60)
