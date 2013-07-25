87cf57f13d6306981c968a3ec2182460808e8fe2
Dan Shea <daniel.john.shea@gmail.com>
2013-07-25 16:20:53 -0400

    20130725: Fixed some issues with exception handling in ChaosMoose and tested a working copy of clean kill method.  Updated .gitignore to ignore pyc files and logs

--- mfs_tests/ChaosMoose.py
+++ mfs_tests/ChaosMoose.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import ConfigParser
 import logging
 import os
@@ -17,12 +18,14 @@ class ChaosMoose(object):
         logfile = '.'.join(['-'.join(['ChaosMoose', time.strftime('%Y%m%d%H%M%S')]), 'log'])
         logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
         self.mastername = mastername
+        self.method = None
+        self.method_args = None
         logging.debug('mastername = {0:s}'.format(mastername))
         # Instantiate a paramiko ssh object
         self.ssh = paramiko.SSHClient()
         logging.debug('instantiated paramiko SSHClient object')
         # Tell the paramiko object to accept unknown keys
-        self.ssh.set_mising_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         logging.debug('set paramiko host key policy to AutoAddPolicy')
 
     def ssh_command(self, host, command):
@@ -36,7 +39,7 @@ class ChaosMoose(object):
         logging.debug('stderr: {0:s}'.format(''.join(error)))
         self.ssh.close()
         logging.debug('Session to {0:s} closed'.format(host))
-        return output
+        return (output, error)
 
     def current_leader(self):
         """Return host and port tuple of the current elected leader of the master nodes or (None, None) if there is no current leader."""
@@ -44,11 +47,11 @@ class ChaosMoose(object):
         options = ' '.join(['-h', self.mastername]) # To be moved to a configuration file
         command = ' '.join([executable, options])
         # ssh to one of the masters to find the leader
-        output = self.ssh_command(self.mastername, command)
+        output, error = self.ssh_command(self.mastername, command)
         leader = None
         port = None
         for line in output:
-            match = re.search('.LEADER.', output)
+            match = re.search('LEADER', line)
             if match:
                 # strip off extraneous bits, leaving us with the ip and the port of the master node
                 leader,port = match.string.split(' : ')[0].split()[1].strip('(').strip(')').split(':')
@@ -62,63 +65,62 @@ class ChaosMoose(object):
         try:
             options = 'stop'                   # To be moved to a configuration file
             command = ' '.join([executable, options])
-            output = self.ssh_command(leader, command)
+            output, error = self.ssh_command(leader, command)
             self.random_sleep()
         except Exception as e:
-            sys.stderr.write(e)
+            sys.stderr.write(e.message)
         finally:
-            # Check if the mfsmaster process is down, if it is down, restart it
-            try:
-                options = 'test'
+                options = 'start'
                 command = ' '.join([executable, options])
-                output = self.ssh_command(leader, command)
-                for line in output:
-                    match = re.search('mfsmaster is not running')
-                    if match:
-                        options = 'start'
-                        command = ' '.join([executable, options])
-                        output = self.ssh_command(leader, command)
-            except Exception as e:
-                sys.stderr.write(e)
+                output, error = self.ssh_command(leader, command)
 
+    #TODO: Fix this so that the ssh interface is not the interface being brought down
     def _network_outage(self, leader, port):
         """Kill the leader by simulating a network outage, wait a bit and then restore the network."""
         try:
             executable = '/sbin/ifdown' # To be moved to a configuration file
             options = 'eth0'            # To be moved to a configuration file
             command = ' '.join([executable, options])
-            output = self.ssh_command(leader, command)
+            output, error = self.ssh_command(leader, command)
             self.random_sleep()
         except Exception as e:
-            sys.stderr.write(e)
+            sys.stderr.write(e.message)
         finally:
             # The interface should always be brought back up, no matter what
-            try:
-                executable = '/sbin/ifup' # To be moved to a configuration file
-                command = ' '.join([executable, options])
-                output = self.ssh_command(leader, command)
-            except Exception as e:
-                sys.stderr.write(e)
+            executable = '/sbin/ifup' # To be moved to a configuration file
+            command = ' '.join([executable, options])
+            output, error = self.ssh_command(leader, command)
     
     '''End of methods of execution'''
     
     def kill_leader(self, leader, port):
         """Kill the current leader using a randomly chosen method of execution."""
-        method_of_execution = [self._clean_kill, self._network_outage] # To be moved to a configuration file
-        random.choice(method_of_execution)(leader, port)
+        method_of_execution = [self._clean_kill] # To be moved to a configuration file
+        self.method = random.choice(method_of_execution)
+        self.method_args=(leader, port)
+        self.method(*self.method_args)
 
     def monitor_election(self):
         """Monitor the status of the election process and return the new leader and port tuple when a new leader has been elected."""
+        leader = None
         while leader == None:
             leader, port = self.current_leader()
+            self.random_sleep(sleep_min=1, sleep_max=5)
         return (leader, port)
     
-    def random_sleep(self, sleep_min=60, sleep_max=3600):
+    def random_sleep(self, sleep_min=60, sleep_max=300):
         """Sleep for a random period of time defined by an inclusive sleep range, given in seconds."""
-        time.sleep(random.randint(sleep_min, sleep_max))
-
-    def signal_handler(signal, frame):
-        sys.stdout.write('Chaos Moose has been contained, but for how long?!?!?\n')
+        sleepval = random.randint(sleep_min, sleep_max)
+        try:
+            logging.info('Going to sleep for {0:d}s'.format(sleepval))
+            time.sleep(sleepval)
+        except Exception as e:
+            sys.stderr.write(e.message)
+        
+    def signal_handler(self, signal, frame):
+        if self.method != None:
+            self.method(*self.method_args)
+        sys.stdout.write('\nChaos Moose has been contained, but for how long?!?!?\n')
         sys.stdout.write('Exiting.\n')
         sys.exit(0)
     
@@ -138,14 +140,18 @@ class ChaosMoose(object):
         '''
         sys.stdout.write(chaos_moose)
         sys.stdout.write('Chaos Moose has been unleashed!!!!!\n')
-        sys.stdout.write('To end his reign of terror, press Ctrl+C\n')
-        signal.signal(signal.SIGINT, signal_handler)
+        sys.stdout.write('      To end his reign of terror, press Ctrl+C\n')
+        signal.signal(signal.SIGINT, self.signal_handler)
         while True:
             # Monitor the election until a new leader has been chosen
             leader, port = self.monitor_election()
             # Kill the current leader
             self.kill_leader(leader, port)
             # Sleep for a random interval of time
-            self.random.sleep()
+            self.random_sleep()
             # Continue to do this until Chaos Moose is sent an Interrupt Signal sending him back to the murky depths from whence he came
     
+if __name__ == '__main__':
+    cm = ChaosMoose()
+    cm.run()
+    sys.exit(0)
